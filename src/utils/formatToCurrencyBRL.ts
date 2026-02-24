57d8bf79a6e4eb8ffb67e8c5b5205422e7bffc61
Bruna Nunes <bruna.f.n@hotmail.com>
2026-02-23 23:23:48 -0300

    Revert "first commit, crud customers"
    This reverts commit 1c4058cafd0b3773cf4559aa04358e0c160c90c6.

--- src/utils/formatToCurrencyBRL.ts
+++
@@ -1,6 +0,0 @@
-const formatToCurrencyBRL = (value: number): string | undefined => {
-    if (!value) return;
-    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
-}
-
-export default formatToCurrencyBRL
