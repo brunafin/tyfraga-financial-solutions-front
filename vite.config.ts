2bb85ac9cca09d3cc37e84b17205fdcbd8febc49
Bruna Nunes <bruna.f.n@hotmail.com>
2026-05-09 20:43:12 -0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,43 +3,57 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
     VitePWA({
       registerType: 'autoUpdate',
+
       injectRegister: 'auto',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+
+      devOptions: {
+        enabled: true,
+      },
+
       manifest: {
         name: 'Tyfraga',
         short_name: 'Tyfraga',
         description: 'Aplicativo Tyfraga',
+
         theme_color: '#ffffff',
         background_color: '#ffffff',
+
         display: 'standalone',
+        orientation: 'portrait',
+
         start_url: '/',
+        scope: '/',
+
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512-maskable.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    })
+          },
+          {
+            src: '/pwa-512x512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+
+      workbox: {
+        navigateFallback: 'index.html',
+      },
+    }),
   ],
 })
